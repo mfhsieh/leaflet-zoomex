44b27b723ddfa2ae076f01798e13374751f2b932
mfhsieh <mfhsieh@gmail.com>
2024-05-23 23:42:01 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Leaflet.Control.ZoomEx
-Description: Leaflet.Control.ZoomEx
+Description: Please refer to the examples [index.html](https://mfhsieh.github.io/leaflet-zoomex/) and [examples/demo.css](https://mfhsieh.github.io/leaflet-zoomex/examples/demo.css).
--- src/leaflet-zoomex.js
+++ src/leaflet-zoomex.js
@@ -15,10 +15,14 @@
 
         options: {
             className: "",
-            zoomOutHtml: "&#8722",  // −
-            zoomInHtml: "&#43;",  // +
-            zoomOutTitle: "Zoom Out",
-            zoomInTitle: "Zoom In",
+            zoomOutHtml: '<svg width="16" height="16" viewBox="0 0 16 16"><path d="M 8,0.5 C 3.8568547,0.5 0.5,3.8568547 0.5,8 c 0,4.143145 3.3568547,7.5 7.5,7.5 4.143145,0 7.5,-3.356855 7.5,-7.5 C 15.5,3.8568547 12.143145,0.5 8,0.5 Z M 4.0080641,9.2096779 c -0.1995968,0 -0.3629027,-0.1633059 -0.3629027,-0.3629027 V 7.1532256 c 0,-0.1995969 0.1633059,-0.3629027 0.3629027,-0.3629027 h 7.9838719 c 0.199597,0 0.362903,0.1633058 0.362903,0.3629027 v 1.6935488 c 0,0.1995969 -0.163306,0.3629027 -0.362903,0.3629027 z" /></svg>',
+            zoomInHtml: '<svg width="16" height="16" viewBox="0 0 16 16"><path d="M 8,0.5 C 3.8568547,0.5 0.5,3.8568546 0.5,7.9999998 0.5,12.143145 3.8568547,15.5 8,15.5 c 4.143145,0 7.5,-3.356855 7.5,-7.5000002 C 15.5,3.8568546 12.143145,0.5 8,0.5 Z m 4.354838,8.3467742 c 0,0.1995969 -0.163306,0.3629027 -0.362902,0.3629027 H 9.2096779 v 2.7822581 c 0,0.199597 -0.1633058,0.362902 -0.3629027,0.362902 H 7.1532256 c -0.1995968,0 -0.3629027,-0.163302 -0.3629027,-0.362902 V 9.2096773 H 4.0080653 c -0.1995968,0 -0.3629027,-0.1633058 -0.3629027,-0.3629027 V 7.1532255 c 0,-0.1995968 0.1633059,-0.3629027 0.3629027,-0.3629027 H 6.7903229 V 4.0080641 c 0,-0.1995969 0.1633059,-0.3629027 0.3629027,-0.3629027 h 1.6935488 c 0.1995969,0 0.3629027,0.1633062 0.3629027,0.3629027 v 2.782258 h 2.7822579 c 0.199597,0 0.362902,0.1633058 0.362902,0.3629027 z" /></svg>',
+            zoomOutTitle: "Map Zoom Out",
+            zoomInTitle: "Map Zoom In",
+            sliderTitle: "Map Zoom In / Zoom Out Slider",
+            zoomOutAriaLabel: "Map Zoom Out",
+            zoomInAriaLabel: "Map Zoom In",
+            sliderAriaLabel: "Map Zoom In / Zoom Out Slider",
             afterZoomEnd: null,
         },
 
@@ -32,7 +36,7 @@
             let container = L.DomUtil.create("div", "leaflet-zoomex");
             if (this.options.className) L.DomUtil.addClass(container, this.options.className);
 
-            this._zoomOutButton = this._createButton(this.options.zoomOutHtml, this.options.zoomOutTitle, "leaflet-zoomex-out", container, this._zoomOut);
+            this._zoomOutButton = this._createButton(this.options.zoomOutHtml, "leaflet-zoomex-out", container, this._zoomOut, this.options.zoomOutTitle, this.options.zoomOutAriaLabel);
 
             this._sliderContainer = L.DomUtil.create("div", "leaflet-zoomex-slider-container", container);
             L.DomEvent.disableClickPropagation(this._sliderContainer);
@@ -41,12 +45,19 @@
             this._slider.type = "range";
             this._slider.min = map.getMinZoom();
             this._slider.max = map.getMaxZoom();
-            this._slider.step = 1;
             this._slider.value = map.getZoom();
+            this._slider.step = 1;
+
+            this._slider.title = this.options.sliderTitle;
+            this._slider.setAttribute("aria-label", this.options.sliderAriaLabel);
+            this._slider.setAttribute("aria-valuemin", this._slider.min);
+            this._slider.setAttribute("aria-valuemax", this._slider.max);
+            this._slider.setAttribute("aria-valuenow", this._slider.value);
+
             L.DomEvent.on(this._slider, "change", this._onSliderInput, this);
             map.on("zoomend", this._updateSlider, this);
 
-            this._zoomInButton = this._createButton(this.options.zoomInHtml, this.options.zoomInTitle, "leaflet-zoomex-in", container, this._zoomIn);
+            this._zoomInButton = this._createButton(this.options.zoomInHtml, "leaflet-zoomex-in", container, this._zoomIn, this.options.zoomInTitle, this.options.zoomOutAriaLabel);
 
             return container;
         },
@@ -65,13 +76,16 @@
 
         _updateSlider: function () {
             this._slider.value = this._map.getZoom();
+            this._slider.setAttribute("aria-valuenow", this._slider.value);
             if (this.options.afterZoomEnd) this.options.afterZoomEnd();
         },
 
-        _createButton: function (innerHTML, title, className, container, callback) {
+        _createButton: function (innerHTML, className, container, callback, title, ariaLabel) {
             let button = L.DomUtil.create("button", className, container);
             button.innerHTML = innerHTML;
+
             button.title = title;
+            button.setAttribute("aria-label", ariaLabel);
 
             L.DomEvent
                 .on(button, "click", L.DomEvent.stopPropagation)
